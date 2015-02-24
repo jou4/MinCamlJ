e36ea1812ab0f312fe136d921d4fb5956a859618
Kazuyoshi Kamitsukasa <jou4.kz@gmail.com>
2015-02-24 12:40:45 +0900

    テスト実行バッチを作成

--- test/test.bat
+++ test/test.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 
 set JAVA_HOME=%JDK8_HOME%
 set JAVA="%JAVA_HOME%"\bin\java
@@ -8,28 +9,34 @@ set COMPILE_OPTS=-cp %BASE_DIR%\build\libs\*
 set COMPILE_MAIN_CLASS=mincamlj.Main
 set RUNTIME_OPTS=-cp classes;%BASE_DIR%\build\libs\*
 set TEST_ML_DIR=ml
+set TEST_OUTPUT_DIR=output
 set TEST_PKG=mincamlj.test
 
 pushd %0\..
 cls
 
-:BUILD
-call %GRADLE% -p %BASE_DIR% build
+REM BUILD
+REM  call %GRADLE% -p %BASE_DIR% build
 
-:COMPILE
-set target=Test
-%JAVA% %COMPILE_OPTS% %COMPILE_MAIN_CLASS% %TEST_ML_DIR%\%target%.ml %TEST_PKG%.%target% .\classes
+if not exist %TEST_OUTPUT_DIR% mkdir %TEST_OUTPUT_DIR%
 
-:TEST
-%JAVA% %RUNTIME_OPTS% %TEST_PKG%.%target% > %TEST_ML_DIR%\%target%.out
-fc %TEST_ML_DIR%\%target%.out %TEST_ML_DIR%\%target%.txt > NUL
-if errorlevel 1 goto ERROR
+for /f "delims=;" %%i in ('dir /b .\ml\*.ml') do call :TEST %%~ni
 echo check done.
 goto END
 
+:TEST
+echo --- %1
+REM  COMPILE
+%JAVA% %COMPILE_OPTS% %COMPILE_MAIN_CLASS% %TEST_ML_DIR%\%1.ml %TEST_PKG%.%1 .\classes
+REM  RUN
+%JAVA% %RUNTIME_OPTS% %TEST_PKG%.%1 > %TEST_OUTPUT_DIR%\%1.out
+REM  CHECK
+fc %TEST_OUTPUT_DIR%\%1.out %TEST_ML_DIR%\%1.txt > NUL
+REM JUMP IF ERROR OCCURED
+if errorlevel 1 echo test failed. (%1.ml) && goto ERROR
+exit /b
+
 :ERROR
-echo error occured.
-echo %errorlevel%
 
 :END
 pause
