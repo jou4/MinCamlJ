fb6121c30abc09bfae43b8f7999f11f5af229122
Kazuyoshi Kamitsukasa <jou4.kz@gmail.com>
2015-02-25 21:31:22 +0900

    floatのテストコードを修正

--- test/ml/float.ml
+++ test/ml/float.ml
@@ -1,6 +1,7 @@
 let a = 2.2 in
 let b = 1.1 in
-let _ = print_float (a +. b) in
-let _ = print_float (a -. b) in
-let _ = print_float (a *. b) in
-print_float (a /. b)
+let c = 3.3 in
+let _ = print_float (a +. b -. c) in
+let _ = print_float (a -. b *. c) in
+let _ = print_float (a *. b +. c) in
+print_float (a /. b *. c)
