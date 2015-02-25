6763f9230d7a03b08b40453cec56823de5c61cb5
Kazuyoshi Kamitsukasa <jou4.kz@gmail.com>
2015-02-25 21:58:03 +0900

    ifのテストコードを追加

---
+++ test/ml/if.ml
@@ -0,0 +1,26 @@
+let a = 10 in
+let b = 20 in
+let c = if a = 10 then 1 else 0 in
+let d = if a = 20 then 1 else 0 in
+let e = if a > 9 then 1 else 0 in
+let f = if a > 10 then 1 else 0 in
+let g = if a >= 10 then 1 else 0 in
+let h = if a >= 11 then 1 else 0 in
+let i = if a < 11 then 1 else 0 in
+let j = if a < 10 then 1 else 0 in
+let k = if a <= 10 then 1 else 0 in
+let l = if a <= 9 then 1 else 0 in
+let m = if a <> 9 then 1 else 0 in
+let n = if a <> 10 then 1 else 0 in
+let _ = print_int c in
+let _ = print_int d in
+let _ = print_int e in
+let _ = print_int f in
+let _ = print_int g in
+let _ = print_int h in
+let _ = print_int i in
+let _ = print_int j in
+let _ = print_int k in
+let _ = print_int l in
+let _ = print_int m in
+print_int n
